a52d45dff888ab7fa04d2b77baf9f2fe3095f1b9
Rahman Yerli <rahmanyerli@gmail.com>
2019-02-17 22:00:34 +1100

    Updates

--- precache-manifest.9d844c4f6d7d21185aab348d8fb5724c.js
+++ precache-manifest.9d844c4f6d7d21185aab348d8fb5724c.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "8e485e29bea66a7e67607676f2365acd",
-    "url": "/static/media/gear.8e485e29.svg"
+    "revision": "618c2838eebecabf0e7ad2bb69422f64",
+    "url": "/static/media/palette.618c2838.svg"
   },
   {
     "revision": "0ee0e855de78716aa9b7",
@@ -16,16 +16,20 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "950307b5c330dc87fdbc1697de0faf31",
-    "url": "/static/media/home.950307b5.svg"
-  },
-  {
     "revision": "86819838dd9f708c3245e3b57de9f1c4",
     "url": "/static/media/logo.86819838.svg"
   },
   {
+    "revision": "950307b5c330dc87fdbc1697de0faf31",
+    "url": "/static/media/home.950307b5.svg"
+  },
+  {
     "revision": "0ee0e855de78716aa9b7",
     "url": "/static/js/main.0ee0e855.chunk.js"
+  },
+  {
+    "revision": "8e485e29bea66a7e67607676f2365acd",
+    "url": "/static/media/gear.8e485e29.svg"
   },
   {
     "revision": "ca46e93e0bc29dd2bb40236659843d71",
@@ -36,15 +40,11 @@ self.__precacheManifest = [
     "url": "/static/media/mixer.86e67b9c.svg"
   },
   {
-    "revision": "618c2838eebecabf0e7ad2bb69422f64",
-    "url": "/static/media/palette.618c2838.svg"
-  },
-  {
     "revision": "1abef5f13fe4b8905dc8a5fea6dbd5b4",
     "url": "/static/media/menu.1abef5f1.svg"
   },
   {
-    "revision": "8f860362e6ced253447ef5ee08ae8267",
+    "revision": "e6f7be17b73b5c142d1220d77ebfeb4a",
     "url": "/index.html"
   }
 ];
